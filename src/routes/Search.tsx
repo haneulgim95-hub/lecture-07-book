ac58564891554d463ab7e0fc6a44ceedfd20b9fd
DeveloperSky95 <haneulgim95@gmail.com>
2026-04-20 09:39:27 +0900

    lecture-07-book 본인 완성

--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -23,15 +23,15 @@ function Search() {
     // useParams를 사용할 떄는 const {id} = useParams();
     // useSearchParams는 useState와 사용법이 동일
     const [params, setParams] = useSearchParams(); // queryString 내용이 params에 담겨 나옴
-    const k = params.get("keyword"); // "수학" 이라는게 있을 수도 있지만, 없을 수도 있음 (k : string | null) >> 쿼리스트링은 사용자가 지울수도 있다.
+    const q = params.get("keyword"); // "수학" 이라는게 있을 수도 있지만, 없을 수도 있음 (k : string | null) >> 쿼리스트링은 사용자가 지울수도 있다.
 
     // keyword 준비 됐으니, API를 통해 요청한 정보를 받아다가 화면에 출력만 해주면 됨
     const [loading, setLoading] = useState(true);
     const [list, setList] = useState<BookItem[]>([]);
 
     useEffect(() => {
-        if (!k) return;
-        fetch(`https://www.googleapis.com/books/v1/volumes?q=${k}&maxResults=20&key=${API_KEY}`)
+        if (!q) return;
+        fetch(`https://www.googleapis.com/books/v1/volumes?q=${q}&maxResults=20&key=${API_KEY}`)
             .then(res => res.json())
             .then(json => {
                 setList(json.items);
@@ -45,7 +45,7 @@ function Search() {
 
     return (
         <div className={styles.container}>
-            <h3>검색 결과: {k}</h3>
+            <h3>검색 결과: {q}</h3>
             <div className={styles.content}>
                 {list.map((value) => (
                     <Link key={value.id} to={`/detail/{value.id}`} className={styles.Link}>
