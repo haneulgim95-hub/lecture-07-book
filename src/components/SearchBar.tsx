d911c7123f3fb2eeb16009cd21b001fb351e98dc
DeveloperSky95 <haneulgim95@gmail.com>
2026-04-20 15:14:25 +0900

    Search 컴포넌트 작성 완료

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,9 +3,14 @@ import { type ChangeEvent, type SubmitEvent, useState } from "react";
 import { useNavigate } from "react-router";
 
 function SearchBar() {
+    // 사용자에게 입력을 받아서, 그걸 /search라고 하는 주소로 강제 이동 시켜야됨
     const navigate = useNavigate(); // navigate라고 하는 변수에 이동에 관련된 기능을 담아둬야 함
     const [keyword, setKeyword] = useState(""); // input에 입력되는 값을 관리할 목적의 state
 
+    // fetch는 순수자바스크립트의 기능이라, Import 필요 없었음
+    // SubmitEvent라고 하는 타입이, 순수 자바스크립트에도 있고 React에도 있음
+    // => import를 안해오면 엔진은 "아 순수자바스크립트꺼?"
+    // => 자동 import가 안됨으로 직접 적어줘서 import 해줘야 한다.
     const onSubmit = (e: SubmitEvent<HTMLFormElement>) => {
         // form에 onSubmit라고 하는 기능은, 브라우저 측에 기본 기능이 "새로고침 하고 전달"이 있음
         // 이걸 하지 못하도록 해야 함.
@@ -15,14 +20,15 @@ function SearchBar() {
         // string의 맨 앞과 맨 뒤에 존재할 수 있는 공백을 제거하는 메서드 : trim()
         // "" 에서 사용자가 enter => keyword.trim() -> ""    -> if (keyword.trim()) -> false
         // " " 에서 사용자가 enter => keyword.trim() -> ""
-        if (!keyword.trim()) return; // trim을 했더니, 값이 빈 스트링이면 return으로 끝내라
+        const k = keyword.trim();
+        if (!k) return; // trim을 했더니, 값이 빈 스트링이면 return으로 끝내라
 
         // keyword를 활용해서 사용자를 강제 이동
         // search라고 하는 경로에, query string으로 keyword를 전달해서 사용자 이동
         // Link는 사용자가 눌러줘야 하는데. useNavigate는 강제 이동이다!
         // URL은 한글이 x, 영어랑 몇 가지의 특수문자만 가능
         // 해석을 할 수 있도록 영어랑 몇 가지의 특수문자를 이용해 변환 작업이 필요함 -> encodeURIComponent()
-        navigate(`/search?keyword=${encodeURIComponent(keyword)}`);
+        navigate(`/search?keyword=${encodeURIComponent(k)}`);
     };
 
     const onChange = (e: ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -10,6 +10,7 @@ type BookItem = {
         title: string;
         authors?: string[];
         description?: string;
+        publishedDate: string;
         imageLinks?: {
             thumbnail?: string;
             small?: string;
@@ -17,41 +18,75 @@ type BookItem = {
     };
 };
 
+type ApiResponseType = { items: BookItem[] };
+
 function Search() {
+    // 이 컴포넌트의 목적 : 사용자가 요청한 keyword를 받아서, 그걸 가지고 google API 요청을 하고, 받아온 결과를 화면에 출력해주는 일
+
     // query string으로 들어온 값을 꺼내오기 위해서는 useSearchParams를 사용
 
     // useParams를 사용할 떄는 const {id} = useParams();
     // useSearchParams는 useState와 사용법이 동일
-    const [params, setParams] = useSearchParams(); // queryString 내용이 params에 담겨 나옴
+    const [params] = useSearchParams(); // queryString 내용이 params에 담겨 나옴
+    // 이렇게 가져온 searchParams라고 하는 state의 값은 객체
+    //search?page=1&size=5&keyword=수학
+    // {page: 1, size: 5, keyword: "수학"}
+    // params.keyword로 값을 가져오면 영어+특수문자의 조합을 가져온다. 따라서 get으로 가져온다.
     const k = params.get("keyword"); // "수학" 이라는게 있을 수도 있지만, 없을 수도 있음 (k : string | null) >> 쿼리스트링은 사용자가 지울수도 있다.
 
     // keyword 준비 됐으니, API를 통해 요청한 정보를 받아다가 화면에 출력만 해주면 됨
-    const [loading, setLoading] = useState(true);
     const [list, setList] = useState<BookItem[]>([]);
 
     useEffect(() => {
         if (!k) return;
+
         fetch(`https://www.googleapis.com/books/v1/volumes?q=${k}&maxResults=20&key=${API_KEY}`)
             .then(res => res.json())
-            .then(json => {
+            .then((json: ApiResponseType) => {
                 setList(json.items);
-                setLoading(false);
             })
             .catch(err => {
                 console.log(err);
-                setLoading(false);
             });
-    }, []);
+    }, [k]);
+
+    // 로딩에 대해서 처리를 굳이 해줘야 하나? => 오히려 loading이 빠르게 지나가면 깜빡꺼리는 느낌이 들고, 사용자는 방금 뭐였지?? 싶은 생각이 든다.
+    // 로딩이 true
+    // if (loading) return <div>Loading...</div>    => 처리를 안해준다! 그래서 이 화면에서 loading이라는 state는 만들지 않았다.
+    // loading이 오래 걸릴것 같고 사용자에게 이러한 안내가 필요할것 같은 상황에만 loading을 만들어준다.
 
     return (
         <div className={styles.container}>
             <h3>검색 결과: {k}</h3>
+
+            {/* 검색 결과 (책 목록) 출력*/}
+            {/* 데이터가 도착했는지 안했는지, 즉 목록이 있는지 없는지 판단 해줘야 하나? */}
+            {/* map이라고 하는 메소드는 꼭 대상이 array여야만 쓸 수 있으니, list && 로 체크해줄 떈,
+            list가 null이 되나?를 생각해봐야겠지만, 여기서 모든 조건을 따+
+            9851740져봤을 때 list는
+            무조건 array이긴 하니깐 굳이 논리곱으로 체긐해줄 필요가 없다. */}
+
             <div className={styles.content}>
-                {list.map((value) => (
-                    <Link key={value.id} to={`/detail/{value.id}`} className={styles.Link}>
-                        <img className={styles.bookImage} src={value.volumeInfo.imageLinks?.thumbnail} alt={value.volumeInfo.title}/>
+                {list.map(value => (
+                    <Link key={value.id} to={`/detail/${value.id}`} className={styles.item}>
+                        {value.volumeInfo.imageLinks?.thumbnail ? (
+                            <img
+                                className={styles.cover}
+                                src={value.volumeInfo.imageLinks?.thumbnail}
+                                alt={value.volumeInfo.title}
+                            />
+                        ) : (
+                            <div className={styles.noCover}>No cover</div>
+                        )}
+
                         <div className={styles.info}>
                             <h3 className={styles.bookTitle}>{value.volumeInfo.title}</h3>
+                            {/*
+                                array에서 사용할 수 있는 메소드 join(문자열)
+                                각 요소를 순회해서 하나의 값을 리턴하는데
+                                각 요소 사이에 [매개변수로 제공된 스트링] 를 넣어준다.
+                            */}
+                            <div className={styles.authors}>{value.volumeInfo.authors?.join(", ")}</div>
                         </div>
                     </Link>
                 ))}
